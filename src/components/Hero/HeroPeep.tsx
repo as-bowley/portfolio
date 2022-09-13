8c6a2adaf4e3907da7fbd5fd913f94c202bcf9cf
as-bowley <as.bowley@gmail.com>
2022-09-13 19:56:32 +0200

    add eye tracking animation to hero img

--- src/components/Hero/HeroPeep.tsx
+++ src/components/Hero/HeroPeep.tsx
@@ -1,10 +1,37 @@
-import React from "react";
-import heropeep from "../../img/hero-peep.png";
+import React, { useEffect } from "react";
+import heropeep from "../../img/peep-51.png";
+import "./HeroPeep.scss";
 
 const HeroPeep: React.FC = () => {
+  useEffect(() => {
+    const eyes = document.querySelectorAll<HTMLElement>(".hero__peep__eye");
+    const peepBg = document.querySelector<HTMLElement>(".hero__peep__bg");
+    window.addEventListener("mousemove", (e) => {
+      eyes.forEach((eye) => {
+        let x = eye.getBoundingClientRect().left + eye.clientWidth / 2;
+        if (eye.classList.contains("eye--right")) {
+          x = eye.getBoundingClientRect().right + eye.clientWidth / 2;
+        }
+        const y = eye.getBoundingClientRect().top + eye.clientHeight / 2;
+        const radian = Math.atan2(e.pageX - x, e.pageY - y);
+        const rot = radian * (180 / Math.PI) * -1 + -90;
+        eye.style.transform = `rotate(${rot}deg)`;
+        // peepBg!.style.translate = `${rot}px, ${rot}px`;
+      });
+    });
+  }, []);
   return (
     <div className="hero__peep">
-      <img src={heropeep} alt="" />
+      <div className="hero__peep__bg"></div>
+      <div className="hero__peep__container">
+        <img src={heropeep} alt="" />
+        <div className="hero__peep__eye--left">
+          <div className="hero__peep__eye--left__pupil  hero__peep__eye"></div>
+        </div>
+        <div className="hero__peep__eye--right">
+          <div className="hero__peep__eye--right__pupil hero__peep__eye"></div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Projects/ProjectModal.tsx
+++ src/components/Projects/ProjectModal.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./ProjectModal.scss";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface Props {
   title: string;
@@ -24,8 +25,28 @@ const ProjectModal: React.FC<Props> = ({
 }) => {
   if (!isModalOpen) return null;
   return (
-    <div className="modal__overlay">
-      <div className="modal__container" onClick={(e) => e.stopPropagation()}>
+    <motion.div
+      className="modal__overlay"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { duration: 0.3, ease: "easeInOut" } }}
+    >
+      <motion.div
+        className="modal__container"
+        onClick={(e) => e.stopPropagation()}
+        initial={{ x: -150, rotateY: 270, scale: 0.8 }}
+        animate={{
+          x: 0,
+          rotateY: 360,
+          scale: 1,
+          transition: { ease: "easeInOut", duration: 0.3 },
+        }}
+        exit={{
+          rotateY: 270,
+          scale: 0.8,
+          x: -150,
+          transition: { ease: "easeInOut", duration: 0.3 },
+        }}
+      >
         <div className="modal__project__content">
           <h2>{title}</h2>
           <img src={previewImg} alt="" className="modal__project__img" />
@@ -59,8 +80,8 @@ const ProjectModal: React.FC<Props> = ({
             ) : null}
           </div>
         </div>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
